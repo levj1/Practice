c647fec89edd7aea2a68f154cb0e742b8ecec4e7
levj1 <levj1@yahoo.com>
2016-08-10 16:10:48 -0400

    Built library class
    Add person class and unit test

---
+++ CF.BL/CF.BL/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CF.BL
+{
+    public class Person
+    {
+        public int ID { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; } 
+
+        public string FullName
+        {
+            get
+            {
+                string output = "";
+                if(!string.IsNullOrWhiteSpace(LastName) && !string.IsNullOrWhiteSpace(FirstName))
+                {
+                    output = LastName + ", " + FirstName;
+                }else if (string.IsNullOrWhiteSpace(LastName))
+                {
+                    output = FirstName;
+                }
+                else if (string.IsNullOrWhiteSpace(FirstName))
+                {
+                    output = LastName;
+                }
+
+                return output;
+            }
+        }
+
+    }
+}
---
+++ CF.BL/CF.Tests/PersonTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CF.BL;
+namespace CF.Tests
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void TestPersonFullName()
+        {
+            // actual
+            Person person = new Person();
+            person.FirstName = "James";
+            person.LastName = "Leveille";
+            string actual = person.FullName;
+
+            // expect
+            string expect = "Leveille, James";
+
+            // assert
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestPersonWithEmptyFirstName()
+        {
+            // actual
+            Person person = new Person();
+            person.FirstName = "";
+            person.LastName = "Leveille";
+            string actual = person.FullName;
+
+            // expect
+            string expect = "Leveille";
+
+            // assert
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestPersonWithEmptyLastName()
+        {
+            // actual
+            Person person = new Person();
+            person.FirstName = "James";
+            person.LastName = "";
+            string actual = person.FullName;
+
+            // expect
+            string expect = "James";
+
+            // assert
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
