54d5964a2e31ebf219ed091457d610837f9fe6ac
levj1 <levj1@yahoo.com>
2016-11-03 11:49:14 -0400

    c# practice
    create classes, learn repository pattern

---
+++ WinBibReadApp/BibRead.BL/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibRead.BL
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Login(string usern, string pass)
+        {
+            UserName = usern;
+            Password = pass;
+        }
+
+        public bool CanLogIn()
+        {
+            bool output = false;
+            
+
+            return output;
+        }
+    }
+}
---
+++ WinBibReadApp/BibRead.BL/RepositoryClass/PersonRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace BibRead.BL.RepositoryClass
+{
+    class PersonRepository
+    {
+        public PersonRepository()
+        {
+            // Create connection
+
+        }
+        // Add a person
+        public void Save(Person person)
+        {
+
+        }
+        // Edit a Person
+
+        // Delete a person 
+
+        // Save a person
+    }
+}
