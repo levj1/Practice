bd54f53772dfbee377f852dc10a2f51de051ee1b
levj1 <levj1@yahoo.com>
2016-11-21 16:07:52 -0500

    database work

--- HandleFile/HandleFile/Form1.cs
+++ HandleFile/HandleFile/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace HandleFile
 {
@@ -15,7 +9,6 @@ namespace HandleFile
     {
         public Form1()
         {
-            InitializeComponent();
         }
 
         private void btnWriteToFile_Click(object sender, EventArgs e)
---
+++ SQLDatabase/SQLDatabase/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string ConnStr = string.Format("");
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnStr))
+                {
+                    conn.Open();
+                    string sql = "select top 10 Associd from association";
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    
+                }
+            }
+            catch (Exception ex )
+            {
+
+                throw;
+            }
+        }
+    }
+}
