6cb9dd0c20e5f67f8134cfdd59fd382153bee747
levj1 <levj1@yahoo.com>
2016-11-22 16:09:05 -0500

    dbconnection - file system

--- SQLDatabase/SQLDatabase/Form1.cs
+++ SQLDatabase/SQLDatabase/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,24 +21,50 @@ namespace SQLDatabase
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            string path = @"C:\Users\James Leveille\Desktop\Temp\test.txt";
             string ConnStr = string.Format("");
 
+            using (SqlConnection conn = new SqlConnection(ConnStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+
+                cmd.CommandText = "Select top 10 * from association";
+                cmd.ExecuteNonQuery();
+
+                string result = "Id - Name - Address1 - Contact";
+                result += Environment.NewLine;
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result += reader[0].ToString() + " - " + reader[1].ToString() + " - " + reader[2].ToString() + " - " + reader[3].ToString();
+                        result += Environment.NewLine;
+                    }
+
+                }
+                WriteToFile(path, result);
+            }
+        }
+
+        private void WriteToFile(string location, string text)
+        {
+            if (!File.Exists(location))
+            {
+                File.Create(location);
+            }
             try
             {
-                using (SqlConnection conn = new SqlConnection(ConnStr))
-                {
-                    conn.Open();
-                    string sql = "select top 10 Associd from association";
-
-                    SqlCommand cmd = new SqlCommand(sql, conn);
-                    
-                }
+                File.WriteAllText(location, text);
             }
-            catch (Exception ex )
+            catch (Exception ex)
             {
 
-                throw;
+                MessageBox.Show(ex.ToString());
             }
+            
         }
     }
 }
