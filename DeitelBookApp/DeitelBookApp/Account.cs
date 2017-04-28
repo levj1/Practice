e713dde2f26d191367c7ac0cbe86e08a107648f0
levj1 <levj1@yahoo.com>
2017-04-28 16:32:06 -0400

    start book deitel

---
+++ DeitelBookApp/DeitelBookApp/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeitelBookApp
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
