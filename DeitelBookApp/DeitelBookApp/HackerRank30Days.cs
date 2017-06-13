6b5e53f7fe146b37a5a0ef31ec6f61bca1b0ddcb
levj1 <levj1@yahoo.com>
2017-06-13 13:15:59 -0400

    day 21

--- DeitelBookApp/DeitelBookApp/HackerRank30Days.cs
+++ DeitelBookApp/DeitelBookApp/HackerRank30Days.cs
@@ -6,6 +6,33 @@ namespace DeitelBookApp
 {
     public static class HackerRank30DaysChallenge
     {
+        public static void D21Generics()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+
+            PrintArray<Int32>(intArray);
+            PrintArray<String>(stringArray);
+        }
+
+        private static void PrintArray<T>(T[] Array)
+        {
+            foreach (var item in Array)
+            {
+                Console.WriteLine(item);
+            }
+        }
 
         public static void D20BubbleSort()
         {
--- DeitelBookApp/DeitelBookApp/Program.cs
+++ DeitelBookApp/DeitelBookApp/Program.cs
@@ -11,11 +11,8 @@ namespace DeitelBookApp
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            string[] a_temp = Console.ReadLine().Split(' ');
-            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
 
-            HackerRank30DaysChallenge.D20BubbleSort(a);
+            HackerRank30DaysChallenge.D21Generics();
             
 
             //DateTime date = Convert.ToDateTime("05/31/2017");
